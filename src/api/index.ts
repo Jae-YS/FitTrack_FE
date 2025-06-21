9d9a97ff51b06efda26275799f65fba7b41e7051
Jae Young Seo <jaeyoungseo@mac.mynetworksettings.com>
2025-06-21 15:46:40 -0400

    changes to the connection

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: import.meta.env.VITE_API_URL, 
+  withCredentials: true, 
 });
 
 export default api;
+
 
 export * from "./auth";
 export * from "./user";
